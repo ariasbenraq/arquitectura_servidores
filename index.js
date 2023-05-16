9dce0c29ab23bc80c520773fdc4c41c800ae673e
Renzo Arias <core.editor>
2023-05-16 17:59:01 -0500

    agregando app.use(express.json) min 30 video 2

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+//Previamente debemos instalar las dependencias:
+// npm install
+// npm install express
+// npm  i -D nodemon
+// code .
+
+
 // requerimos express
 const express = require('express')
 const app = express()
+
+
+//app.use(express.urlencoded({extended: false})); // peticiones codificadas como formulario (x-form-urlencoded)
+app.use(express.json());
 
 
 // Definimos rutas
@@ -19,11 +30,22 @@ const app = express()
 // Como hacemos el renderizado de vista? el devolver un json?
 // Se utilizara el objeto res
 
-app.get('/', (req, res) => {
+
+// ruta comodin :id whilecard
+app.get('/users/:identifier/', (req, res) => {
     console.log("here I am at /");
 
     // Console.log de los query params
-    console.log('query params',req.query);
+    console.log('query params', req.query);
+
+    //Como puedo hacer que vengan parametros en el path?
+    //Definiendo la ruta con parametros
+    console.log('path params', req.params);
+
+    //Quê otra forma tenemos de recibir dqtos del cliente?
+    //En las cabeceras
+    console.log("headers", req.headers)
+
 
     //El metodo send permitira enviar una respuesta
     //Con el objeto res, tambien podemos forzar el status "res.status"
@@ -42,13 +64,25 @@ app.get('/', (req, res) => {
     // Podemos utilizar el metodo 'json'
 
     // res.json({foo:'bar'})
-    res.status(404).json({message:"not found"});
+    res.status(404).json({ message: "not found" });
 
-})
+});
 
-app.get('/users', (req, res) => {
+app.post('/users/:identifier/', (req, res) => {
+    console.log("here I am at /");
 
-})
+    //parametros que puedo recibir del cliente 
+
+    console.log('query params', req.query);
+    console.log('path params', req.params); //parametros en la url
+    console.log("headers", req.headers); //cabeceras
+    console.log("body", req.body); //cuerpo del mensaje post, previamente debemos hacer un app.use(express.json) para que express sepa decodificar el body de las peticiones
+
+    res.status(200).json({ message: "ping" });
+
+});
+
+
 
 app.get('/about', (req, res) => {
 
